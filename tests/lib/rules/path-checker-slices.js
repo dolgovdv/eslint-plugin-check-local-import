4c563b709d75812824cc7ed271d27dafdfd84101
ddolgov <dolgov.d.v@yandex.ru>
2024-05-15 19:09:45 +0300

    refactor path-checker-slices

--- tests/lib/rules/path-checker-slices.js
+++ tests/lib/rules/path-checker-slices.js
@@ -17,13 +17,13 @@ const rule = require("../../../lib/rules/path-checker-slices"),
 //------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester({parserOptions: {ecmaVersion: 6, sourceType: "module"},});
-ruleTester.run("slices", rule, {
+ruleTester.run("Check local import into slices", rule, {
     valid: [
       {
         filename: '\\\\wsl$\\Ubuntu\\home\\plugin\\registry\\src\\features\\TestCheck\\TestCheck.ts',
         code: "import {InternalChecksClient} from '../TestCheck/ExportSegment'",
         errors: [],
-        options: [{rootPath: 'src', slices: ['features', 'entities']}]
+        options: [{rootPath: 'src', layers: ['features', 'entities']}]
       },
     ],
 
@@ -32,13 +32,13 @@ ruleTester.run("slices", rule, {
             filename: '\\\\wsl$\\Ubuntu\\home\\plugin\\registry\\src\\features\\TestCheck\\TestCheck.ts',
             code: "import {InternalChecksClient} from 'features/TestCheck/ExportSegment'",
             errors: [{message: "Inside the slices module, the import must be relative."}],
-            options: [{rootPath: 'src', slices: ['features', 'entities']}]
+            options: [{rootPath: 'src', layers: ['features', 'entities']}]
         },
       {
         filename: '\\\\wsl$\\Ubuntu\\home\\plugin\\registry\\src\\features\\TestCheck\\TestCheck.ts',
         code: "import {InternalChecksClient} from 'features/TestCheck'",
         errors: [{message: "Inside the slices module, the import must be relative."}],
-        options: [{rootPath: 'src', slices: ['features', 'entities']}]
+        options: [{rootPath: 'src', layers: ['features', 'entities']}]
       },
     ],
 });
